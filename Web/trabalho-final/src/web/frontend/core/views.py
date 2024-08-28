a4fcfe3ba5480f2c9b7f81477d3e714df4a33ef2
keven silva <keven.almeida.8179@gmail.com>
2024-08-28 18:44:58 -0300

    Adicionando tela de dashboard

--- Web/trabalho-final/src/web/frontend/core/views.py
+++ Web/trabalho-final/src/web/frontend/core/views.py
@@ -8,8 +8,15 @@ from src.utils.shortcuts import render
 router = APIRouter()
 
 
+@router.get("/dashboard", response_class=HTMLResponse, tags=["core"])
+async def get_dashboard_page(
+    request: Request,
+) -> HTMLResponse:
+    return render(request, "dashboard/pages/dashboard.html", context={})
+
+
 @router.get("/problem", response_class=HTMLResponse, tags=["core"])
 async def get_problem_page(
     request: Request,
 ) -> HTMLResponse:
-    return render(request, "global/pages/problem.html", context={})
+    return render(request, "problem/pages/problem.html", context={})
---
+++ Web/trabalho-final/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          lg: '1200px',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
