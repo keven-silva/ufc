ea6defb131a60e54b93ab0e9348a913e8754040a
keven silva <keven.almeida.8179@gmail.com>
2024-08-28 23:34:54 -0300

    Adicionando telas de problema, causa e impacto da dengue

--- Web/trabalho-final/src/web/frontend/core/views.py
+++ Web/trabalho-final/src/web/frontend/core/views.py
@@ -15,8 +15,22 @@ async def get_dashboard_page(
     return render(request, "dashboard/pages/dashboard.html", context={})
 
 
-@router.get("/problem", response_class=HTMLResponse, tags=["core"])
+@router.get("/problems", response_class=HTMLResponse, tags=["core"])
 async def get_problem_page(
     request: Request,
 ) -> HTMLResponse:
     return render(request, "problem/pages/problem.html", context={})
+
+
+@router.get("/causes", response_class=HTMLResponse, tags=["core"])
+async def get_cause_page(
+    request: Request,
+) -> HTMLResponse:
+    return render(request, "cause/pages/cause.html", context={})
+
+
+@router.get("/impacts", response_class=HTMLResponse, tags=["core"])
+async def get_impact_page(
+    request: Request,
+) -> HTMLResponse:
+    return render(request, "impact/pages/impact.html", context={})
